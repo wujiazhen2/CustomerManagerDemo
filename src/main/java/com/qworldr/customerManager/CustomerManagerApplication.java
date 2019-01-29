a8a341ddd54db3c59561098df9d78082a18b2ca4
wujiazhen22 <1599153675@qq.com>
2019-01-29 14:59:41 +0800

    first commit

---
+++ src/main/java/com/qworldr/customerManager/CustomerManagerApplication.java
@@ -0,0 +1,14 @@
+package com.qworldr.customerManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerManagerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/qworldr/customerManager/CustomerManagerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.qworldr.customerManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
